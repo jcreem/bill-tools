4d5f28d07d38372c58240aeed7c289f12bcaaead
me@mail.com <me@mail.com>
2016-03-11 21:58:25 -0500

    works to login

--- gs_collab_sheet.py
+++ gs_collab_sheet.py
@@ -3,7 +3,8 @@ import gspread
 import argparse
 import json
 
-from oauth2client.client import SignedJwtAssertionCredentials
+#from oauth2client.service_account import ServiceAccountCredentials
+from oauth2client.client import GoogleCredentials
 
 parser = argparse.ArgumentParser(description="")
 parser.add_argument("url")
@@ -12,7 +13,9 @@ args = parser.parse_args()
 json_key = json.load(open('NHLAGS-e8b3911072d5.json'))
 scope = ['https://spreadsheets.google.com/feeds']
 
-credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'].encode(), scope)
+#credentials = ServiceAccountCredentials(json_key['client_email'], json_key['private_key'].encode(), scope)
+credentials = GoogleCredentials.get_application_default()
+credentials = credentials.create_scoped(scope)
 
 gss_client = gspread.authorize(credentials)
 
